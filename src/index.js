4090ce062eac19e0ad56231132df1ce7de910f03
mansurissa <mansurissa6@gmail.com>
2020-10-16 18:14:46 +0200

    installing and using helmet to hide headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config.js";
 import express from "express";
 import passport from "passport";
+import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import upload from "express-fileupload";
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(upload({ useTempFiles: true }));
 app.use(cors());
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(passport.initialize());
 passport.use(jwtStrategy);
